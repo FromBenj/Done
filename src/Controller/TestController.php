2ade7d3f44bfb4dff9de04de1bc28d9eafcecfc9
Benjamin Touboul <touboul.benjamin@gmail.com>
2021-02-02 17:56:44 +0100

    initial commit

---
+++ src/Controller/TestController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TestController extends AbstractController
+{
+    /**
+     * @Route("/test", name="test")
+     */
+    public function index(): Response
+    {
+        $date = date('l');
+        return $this->render('test/index.html.twig', [
+            'controller_name' => 'TestController',
+            'date' => $date,
+        ]);
+    }
+}
